69e94abd499fc789a9241fa81806da397663cb69
estrelladosa <eds21@alu.ua.es>
2025-03-25 17:18:09 +0100

    instaladas todas las dependencias y añadido header a la pagina de inicio

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,10 +23,12 @@ import ReactDOM from "react-dom/client";
 import './index.css';
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <Header />
     <App />
   </BrowserRouter>
 );
